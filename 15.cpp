a8ff58831684ea7eb009900b718c3cfc40bae333
Daren11031105 <104020738+Daren11031105@users.noreply.github.com>
2022-07-04 12:56:11 +0800

    練習題

---
+++ 15.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define int long long
+#define v vector<int>
+using namespace std;
+v F(int n)
+{
+    assert(n >= 0 && n <= 200);
+    v ans;
+    ans.push_back(1);
+    for (int i = 1; i <= n; ++i)
+    {
+        int b = 0;
+        for (auto &c : ans)
+        {
+            int product = c * i + b;
+            c = product % 10000;
+            b = product / 10000;
+        }
+        if (b > 0)
+        {
+            ans.push_back(b);
+        }
+    }
+    return ans;
+}
+void print(const v &num)
+{
+    if (num.empty())
+        puts("0");
+    else
+    {
+        cout << num.back();
+        for (auto it = num.rbegin() + 1; it != num.rend(); ++it)
+            printf("%04d", *it);
+        cout << '\n';
+    }
+}
+signed main()
+{
+    v ans = F(200);
+    print(ans);
+}
