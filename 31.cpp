707758b6b30002ee2101f97023b583c4c9f384d0
Daren11031105 <104020738+Daren11031105@users.noreply.github.com>
2022-07-04 20:24:48 +0800

    練習題

--- 31.cpp
+++ 31.cpp
@@ -7,10 +7,15 @@ void sol()
     vector<int> a(n), b(n);
     for (int i = 0; i < n; ++i) cin >> a[i];
     for (int i = 0; i < n; ++i) cin >> b[i];
+    cout << '\n';
     for (int i = 0; i < n; ++i) swap(a[i], b[i]);
     cout << "After swaped:\nA:";
     for (int i = 0; i < n; ++i) cout << a[i] << " ";
     cout << "\nB:";
     for (int i = 0; i < n; ++i) cout << b[i] << " ";
     cout << '\n';
+}
+int main()
+{
+    sol();
 }
