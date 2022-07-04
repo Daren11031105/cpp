d8cc16dbeadf8c1b58d23bd70c8a2a4b91fb9c85
Daren11031105 <104020738+Daren11031105@users.noreply.github.com>
2022-07-04 08:38:00 +0800

    練習題

---
+++ 27.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    while (cin >> a >> b)
+    {
+        cout << __gcd(a, b) << '\n';
+    }
+    
+    return 0;
+}
