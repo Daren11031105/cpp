4c0b4b72d7e735e791daef15f0dcf87a704a76e7
Daren11031105 <104020738+Daren11031105@users.noreply.github.com>
2022-07-04 12:21:55 +0800

    練習題

---
+++ 30.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[5][3], sum;
+int main()
+{
+    for (int i = 0; i < 5; ++i)
+        for (int j = 0; j < 3; ++j) {cin >> a[i][j]; sum += a[i][j];}
+    cout << sum << '\n';
+}
